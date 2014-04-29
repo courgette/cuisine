8c48095545662e383703376994fe28ec89ac3305
Thomas LEBEAU <thomas.lebeau@g-nova.fr>
2014-04-29 18:36:46 +0200

    add jade + init index.jade

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 var express = require('express'),
-    app = express();
+    routes = require('./routes');
+
+var app = module.exports = express();
 
 app.configure(function() {
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'jade');
     app.use(express.static(__dirname + '/public'));         // set the static files location /public/img will be /img for users
     app.use(express.logger('dev'));                         // log every request to the console
     app.use(express.bodyParser());                          // pull information from html in POST
@@ -9,3 +13,5 @@ app.configure(function() {
 });
 
 app.listen(3000);
+
+app.get('/', routes.index);
